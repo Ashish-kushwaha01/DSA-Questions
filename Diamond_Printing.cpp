4a62948e434b8436f4aa16d0007cb9950fd5811a
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-13 00:38:52 +0530

    Diamond Pattern

---
+++ Diamond_Printing.cpp
@@ -0,0 +1,23 @@
+/* Diamond Printing .*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter that what is the mid length of your Diamond : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++)
+        cout<<" ";
+        for(int j=1;j<=i;j++)
+        cout<<"* ";
+        cout<<"\n";
+
+    }
+    for(int i=1;i<=n-1;i++){
+        for(int j=i-1;j>=1;j--)
+        cout<<" ";
+        for(int j=n-1;j>=i;j--)
+        cout<<" *";
+        cout<<"\n";
+    }
+}
