ab1567b860fe63e7eaf7eae27c10ff9a9d8b9930
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-13 00:19:00 +0530

    Left triangle with upper  Pyramid

--- Pattern_Left_Triangle.cpp
+++ Pattern_Left_Triangle.cpp
@@ -14,4 +14,14 @@ int main(){
 
         cout<<"\n";
     }
+
+
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++)
+        cout<<" ";
+        for(int j=1;j<=i;j++)
+        cout<<"* ";
+
+        cout<<"\n";
+    }
 }
