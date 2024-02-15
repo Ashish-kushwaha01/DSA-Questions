0e840f32f36ee021820aed9f97414528ba5e4f80
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-15 22:58:55 +0530

    Selection sort

--- Selection_Sort.cpp
+++ Selection_Sort.cpp
@@ -1,6 +1,16 @@
 /* Sorting of Array using selection sort .  */
 #include<iostream>
 using namespace std;
+// int Min(int arr[],int n){
+//     int min=arr[0];
+//     for(int i=0;i<n;i++){
+//         if(arr[i]<min)
+//         min=arr[i];
+        
+//     }
+//     return min;
+
+// }
 int main(){
     int n;
     cout<<"Enter that how many no. you have ? : ";
@@ -18,20 +28,30 @@ int main(){
 
     cout<<"Your no. are : ";
     for(int i=0;i<n;i++){
-        cout<<arr[i];
+        cout<<arr[i]<<" ";
     }
     cout<<"\n";
-
+    
     for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]>arr[j]){
-                int temp=arr[i];
-                arr[i] = arr[j];
-                arr[j]= temp;
-            }
-        }
-    }
+        // int min=arr[i];
+        int min=arr[i];
+        int index=i;
+        //5 4 3 2 1
+        //    int min= Min(arr[n],n);
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<min)
+                index=j;
+                min=arr[index];
 
+            // 5 4 3 2 1 
+            
+        }
+        if(arr[i]>min){
+       int temp=arr[i];
+            arr[i]=arr[index];
+            arr[index]=temp;
+    }
+    }
     cout<<"\n";
     cout<<"After sorting my elements are : ";
     for(int i=0;i<n;i++){
