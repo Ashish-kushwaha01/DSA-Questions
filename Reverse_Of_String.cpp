b5636f733738fc9e2af51b6b3c680e76f2f0e8cd
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-18 21:04:33 +0530

    Reverse of My string

---
+++ Reverse_Of_String.cpp
@@ -0,0 +1,16 @@
+/* Reverse of a string .*/
+#include<iostream>
+#include<string.h>
+#include<stdlib.h>
+using namespace std;
+int main(){
+    string s;
+    cout<<"Enter your string : ";
+    cin>>s;
+
+    int len=s.length();
+    string p = "";
+    for(int i=0;i<len;i++){
+        cout<<cha;
+    }
+};
