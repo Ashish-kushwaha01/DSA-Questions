8e7f73eb6eed08d1a5c8c5305e6d99a58be173e1
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-20 19:40:36 +0530

    Decreasing order of my array element by using Insertion sort

---
+++ Insertion_Sort_Decreasing.cpp
@@ -0,0 +1,28 @@
+/* Decreasing order of my array using inertion sort . */
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[1000];
+    int n;
+    cout<<"How many no. you have ? : ";
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cout<<"Enter no. "<<i+1<<" : ";
+        cin>>arr[i];
+    }
+
+    for(int i=1;i<n;i++){
+        for(int j =i;j>0;j--){
+            if(arr[j]>arr[j-1])
+            swap(arr[j],arr[j-1]);
+            else 
+            break;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<"  ";
+    }
+}
+gh
