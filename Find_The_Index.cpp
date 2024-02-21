58b5e47ce87db1e5c4230b26b55007e09dec959d
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-21 20:59:52 +0530

    Index of entered value

---
+++ Find_The_Index.cpp
@@ -0,0 +1,25 @@
+/* Find the index of my given element in a sorted array */
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={1,4,5,6,8,10,11};
+    int mid;
+    int start=0;
+    int end=5;
+    int k;
+    cout<<"Enter your target value  : ";
+    cin>>k;
+cout<<"\n";
+    while(start<=end){
+        mid=start+(end-start)/2;
+        if(arr[mid-1]<=k && arr[mid]>=k)
+        {
+            cout<<mid;
+            break;
+        }
+        else if(arr[mid]>k)
+        end=mid-1;
+        else
+        start=mid+1;
+    }
+}
