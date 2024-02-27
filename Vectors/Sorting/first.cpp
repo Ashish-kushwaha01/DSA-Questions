5b6c34bdd2da8f5bbeb3b562d40bb98c6a4620af
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-27 10:55:12 +0530

    Sorting of vectors

--- Vectors/Sorting/first.cpp
+++ Vectors/Sorting/first.cpp
@@ -26,8 +26,47 @@ cout<<"\n\n";
      sort (arr.rbegin(),arr.rend());
      for(int i=0;i<arr.size();i++)
         cout<<arr[i]<<" ";
+     cout<<"\n";
      
-    
+
+     // Creating second vector 
+
+     int n;
+     cout<<"Enter how many do you want to take : " ;
+     cin>>n;
+     cout<<"\n";
+     vector<int>b(n);
+    for(int i=0;i<n;i++){
+        cout<<"Enter no. "<<i+1<<" : ";
+        cin>>b[i];
+    }
+
+    cout<<"This is your given elements : ";
+    for(int i=0;i<n;i++){
+        
+        cout<<b[i]<<" ";
+    }
+
+         cout<<"\n";
+
+         cout<<"After sorting my elements in increasing order : ";
+         sort(b.begin(),b.end());
+         for(int i=0;i<n;i++){
+        
+        cout<<b[i]<<" ";
+    }
+         cout<<"\n";
+     cout<<"\n";
+
+
+cout<<"After sorting my elements in decreasing order : ";
+         sort(b.rbegin(),b.rend());
+         for(int i=0;i<n;i++){
+        
+        cout<<b[i]<<" ";
+    }
+
+
 
      return 0;
 }
