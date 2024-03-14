d5c9560a7312ad80358ebfce98b11097f73e95c8
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-03-14 22:39:32 +0530

    Rotate my matrix at 90 degree in anti-clock wise

---
+++ 2DArray/Rotate_2_90.cpp
@@ -0,0 +1,43 @@
+/* Rotate matrix by 90 degree in anti-clock wise . */
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<limits.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter order of your square  matrix : ";
+    cin>>n ;
+
+    
+
+    int arr[300][300];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+        cout<<"Enter element ["<<i+1<<","<<j+1<<"] : ";
+            cin>>arr[i][j];
+        }
+    }
+
+    cout<<"\n\n";
+    cout<<"Your matrix is : \n";
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+           cout<<arr[i][j]<<" ";
+        }
+        cout<<" \n";
+    }
+
+
+    cout<<"After rotation at 90 degree in anti-clock wise : \n";
+    for(int j=n-1;j>=0;j--){
+        for(int i=0;i<n;i++){
+            cout<<arr[i][j]<<" ";
+
+        } 
+        cout<<"\n";
+    }
+
+
+    return 0;
+}
