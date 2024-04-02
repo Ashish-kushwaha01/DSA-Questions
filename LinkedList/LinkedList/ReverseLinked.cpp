6bc4abec4aa6f10c21080fc8102aa251d4599dd9
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-04-02 14:53:20 +0530

    Reverse of a linked list

---
+++ LinkedList/LinkedList/ReverseLinked.cpp
@@ -0,0 +1,53 @@
+/* Write a program to print the reverse of my linked list . */
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<limits.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int value){
+        data = value;
+        next = NULL;
+    }
+};
+
+Node *CreateLinkedList(int arr[],int index,int size){
+    if(index == size)
+    return NULL;
+    Node *temp;
+    temp = new Node(arr[index]);
+    temp->next = CreateLinkedList(arr,index+1,size);
+    return temp;
+
+}
+int main(){
+    int arr[] ={ 4,5,6,7,8};
+
+    Node *Head= CreateLinkedList(arr,0,5);
+
+    Node *curr = Head;
+    Node *prev = NULL;
+    Node *fut = Head->next;
+
+    while(fut){
+        prev= curr;
+        curr = fut;
+        fut = curr->next;
+        curr->next = prev;
+    }
+
+    Head->next = NULL;
+    Head = curr;
+    
+    Node *temp = Head;
+
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+
+}
