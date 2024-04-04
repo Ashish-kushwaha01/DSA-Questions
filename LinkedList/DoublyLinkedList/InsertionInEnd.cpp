0ee11d00e1795d484f57468ab2318509116344d3
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-04-04 23:28:08 +0530

    Insertion in the End of a doubly linked list

---
+++ LinkedList/DoublyLinkedList/InsertionInEnd.cpp
@@ -0,0 +1,53 @@
+/* Write a program to insert any element in the end of the doubly linked list . */
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<limits.h>
+using namespace std;
+
+class Node{
+    public:
+    int data ;
+    Node* next;
+    Node* prev;
+
+    Node(int value){
+        data = value;
+        next =NULL;
+        prev =NULL;
+    }
+};
+int main(){
+
+    int arr[] = {3,4,5,6,7};
+
+    Node *Head = NULL;
+    Node *point = Head;
+     for(int i = 0;i<5;i++){
+    if(Head == NULL){
+        Head = new Node(arr[i]);
+        point = Head;
+    }
+    else {
+   
+        Node *temp = new Node(arr[i]);
+        point->next = temp;
+        temp->prev = point;
+        point = temp;
+
+    }
+    }
+    Node *add = new Node(34);
+        point->next =  add;
+         add->prev = point;
+        point =  add;
+
+
+
+    Node *print=Head;
+    while(print){
+        cout<<print->data<<"  ";
+        print= print->next;
+    }
+    return 0;
+}
